be99ad5aa6fec5efa0c9ea74858439d85a30ce2a
Edward-Curry <ecurry@tcd.ie>
2026-05-20 14:51:34 +0100

    Add sparse matrix multiplication test

---
+++ tests/test_sparse_matrix.cpp
@@ -0,0 +1,38 @@
+#include "common/sparse_matrix.hpp"
+
+#include <cassert>
+#include <iostream>
+
+int main()
+{
+    gmres::Vector values = {
+        2.0,
+        3.0, 4.0,
+        5.0, 6.0
+    };
+
+    std::vector<gmres::Index> col_indices = {
+        0,
+        1, 2,
+        0, 2
+    };
+
+    std::vector<gmres::Index> row_ptr = {
+        0, 1, 3, 5
+    };
+
+    gmres::SparseMatrixCSR A(3, 3, values, col_indices, row_ptr);
+
+    gmres::Vector x = {1.0, 2.0, 3.0};
+
+    gmres::Vector y = A.multiply(x);
+
+    assert(y.size() == 3);
+    assert(y[0] == 2.0);
+    assert(y[1] == 18.0);
+    assert(y[2] == 23.0);
+
+    std::cout << "Sparse matrix test passed.\n";
+
+    return 0;
+}

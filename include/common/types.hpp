dffdb7abd35e4d34dfcedb2c89f3ee68d3a19fe4
Edward-Curry <ecurry@tcd.ie>
2026-05-20 13:10:50 +0100

    defined commone types for easy change later on in types.hpp

---
+++ include/common/types.hpp
@@ -0,0 +1,14 @@
+#ifndef COMMON_TYPES_HPP
+#define COMMON_TYPES_HPP
+
+#include <vector>
+#include <cstddef>
+
+namespace gmres
+{
+    using Scalar = double;
+    using Index = std::size_t;
+    using Vector = std::vector<Scalar>;
+}
+
+#endifcmak
